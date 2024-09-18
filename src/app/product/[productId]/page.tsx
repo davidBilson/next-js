623e6886af0c89e25369a2d887285aa2a5cf6e56
David Bilson <davidbilson99@gmail.com>
2024-09-18 20:47:55 +0100

    episodes 19

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function ProductDetails({ params }: {
     params: {productId: string}
 }) {
@@ -5,6 +7,9 @@ export default function ProductDetails({ params }: {
         <>
             <h1>Details about product {params.productId}</h1>
             <h4>Products page.tsx expected in layout</h4>
+            <Link href={"1"}>Product 1</Link>
+            <Link href={"2"}>Product 2</Link>
+            <Link href={"3"}>Product 3</Link>
         </>
     )
 }
